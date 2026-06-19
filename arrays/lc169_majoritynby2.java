790631f7d499911c70641712b819341f9164e1a0
sumit kale <kalesumit104@gmail.com>
2026-06-19 20:57:46 +0530

    changes in 169

--- arrays/lc169_majoritynby2.java
+++ arrays/lc169_majoritynby2.java
@@ -25,6 +25,28 @@ class Solution {
     }
 }
 
+//another simple way
+
+      class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+}
 // if(num==candidate) count++;
 // else if(count==0) candidate=num;
 // else count--;
@@ -49,3 +71,20 @@ class Solution {
 
 // This works because majority element cannot be completely canceled.
 // Idea: Majority element appears more than n/2 times, so it will survive all cancellations.
+[2, 2, 1, 1, 1, 2, 2]
+num    candidate	count
+2	    2	         1
+2	    2	         2
+1	    2	         1
+1	    2	         0
+1	    1	         1
+2	    1	         0
+2	    2	         1
+
+Final answer = 2
+
+In general:
+
+If two different elements meet, they cancel each other.
+Since the majority element appears more than n/2 times, there are not enough other elements to completely cancel it.
+Therefore, after all possible cancellations, the majority element is the only one that can survive.
