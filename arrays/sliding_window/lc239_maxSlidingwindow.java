06cb9a6bb36b6046cb353beff42ef24d7f92c657
sumit kale <kalesumit104@gmail.com>
2026-07-07 20:11:55 +0530

    239

---
+++ arrays/sliding_window/lc239_maxSlidingwindow.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n=nums.length;
+        PriorityQueue<Integer>pq=new PriorityQueue<>(Collections.reverseOrder());
+        List<Integer>ans=new ArrayList<>();
+        int[] a=new int[n-k+1];
+
+        for(int i=0;i<=n-k;i++){
+            for(int j=i;j<i+k;j++){
+                pq.add(nums[j]);
+              
+            }
+            a[i]=pq.poll();
+              while(!pq.isEmpty()){
+                    pq.poll();
+                }
+            
+
+
+        }
+        return a;
+        
+
+       
+        }
+
+    }
+
+    
